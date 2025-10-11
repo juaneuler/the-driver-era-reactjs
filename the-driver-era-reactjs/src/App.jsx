3cbd894738d16850de77faa337c946f035c20c97
Juan Euler <juaneuler@hotmail.com>
2025-10-11 16:32:41 -0300

    feat(routing): se agregó LoaderProvider y AppRoutes para manejar loader en cambios de ruta

--- the-driver-era-reactjs/src/App.jsx
+++ the-driver-era-reactjs/src/App.jsx
@@ -1,36 +1,61 @@
-// Componentes: barra de navegación, footer, React Router DOM, carrito y contenedores
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HelmetProvider } from "react-helmet-async"
-import NavBar from "./components/NavBar"
-import ItemListContainer from "./components/ItemListContainer"
-import ItemDetailContainer from "./components/ItemDetailContainer"
-import Footer from "./components/Footer"
+// Componentes principales
+
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
+import ItemDetailContainer from './components/ItemDetailContainer'
+import Footer from './components/Footer'
 import NotFound from './components/NotFound'
-import CartProvider from './context/CartProvider'
 import Cart from './components/Cart'
 import Checkout from './components/Checkout'
 
+// Contextos
+
+import CartProvider from './context/CartProvider'
+import { LoaderProvider, useLoader } from './context/LoaderProvider'
+
+// Subcomponente que reacciona al cambio de ruta
+
+function AppRoutes() {
+  const location = useLocation()
+  const { showLoader } = useLoader()
+
+  useEffect(() => {
+    showLoader()
+  }, [location])
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/detail/:slug" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+// App principal
+
 function App() {
   return (
-    <div>
-      <HelmetProvider>
-        <CartProvider>
+    <HelmetProvider>
+      <CartProvider>
+        <LoaderProvider>
           <BrowserRouter>
-            <NavBar />
-            <Routes>
-              <Route path='/' element={<ItemListContainer />}></Route>
-              <Route path='/category/:categoryId' element={<ItemListContainer />}></Route>
-              <Route path='/detail/:slug' element={<ItemDetailContainer />}></Route>
-              <Route path='/cart' element={<Cart />}></Route>
-              <Route path='/checkout' element={<Checkout />}></Route>
-              <Route path='*' element={<NotFound />}></Route>
-            </Routes>
-            <Footer />
+            <AppRoutes />
           </BrowserRouter>
-        </CartProvider>
-      </HelmetProvider>
-    </div>
+        </LoaderProvider>
+      </CartProvider>
+    </HelmetProvider>
   )
 }
 
