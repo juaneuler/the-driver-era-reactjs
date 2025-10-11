71a3d481c97a62e86ec2b57fae214f12c425acfa
Juan Euler <juaneuler@hotmail.com>
2025-10-11 16:33:50 -0300

    feat(cart): se integró LoaderProvider y pantalla de carga en el componente Cart

--- the-driver-era-reactjs/src/components/Cart.jsx
+++ the-driver-era-reactjs/src/components/Cart.jsx
@@ -1,8 +1,12 @@
-import React, { useContext, useMemo } from 'react'
+import React, { useContext, useMemo, useEffect, useState } from 'react'
 import { Cart as ContextoCarrito } from '../context/CartProvider'
 import { NavLink } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async';
-import CartItem from './CartItem';
+import CartItem from './CartItem'
+
+// Loader global
+import { useLoader } from '../context/LoaderProvider'
+import PantallaNegra from './PantallaNegra'
 
 // Hoja de estilos
 import "../styles/cart.scss"
@@ -10,18 +14,36 @@ import "../styles/cart.scss"
 const Cart = () => {
 
     const { carrito, vaciarCarrito, eliminarProducto } = useContext(ContextoCarrito)
+    const { showLoader, hideLoader } = useLoader()
+    const [cargando, setCargando] = useState(true)
 
     const precioTotal = useMemo(() => {
         return carrito.reduce((acumulador, item) => acumulador + item.precio * item.cantidad, 0)
     }, [carrito])
+
+    // Efecto para simular carga inicial y disparar loader
+    useEffect(() => {
+        showLoader()
+        setCargando(true)
+
+        const timer = setTimeout(() => {
+            setCargando(false)
+            hideLoader()
+        }, 1500)
+
+        return () => clearTimeout(timer)
+    }, [])
+
+    // Mientras carga, mostramos PantallaNegra
+    if (cargando) return <PantallaNegra />
 
     return (
         <>
             <Helmet>
                 <title>Carrito | THE DRIVER ERA SHOP</title>
             </Helmet>
-            <div className='contenedorCarrito'>
-                <h1>PRODUCTOS</h1>
+            <div className={`contenedorCarrito ${carrito.length === 0 ? 'vacio' : ''}`}>
+                <h1 className="tituloCarrito">PRODUCTOS</h1>
                 {carrito.length ? (
                     <>
                         {carrito.map((itemCarrito) => (
